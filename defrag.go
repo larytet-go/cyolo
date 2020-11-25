a4daed649daffa14fc0b2cc71c6cad7d02b78456
arkady <larytet@yahoo.com>
2020-11-25 17:33:52 +0200

    Update

--- defrag.go
+++ defrag.go
@@ -1,13 +1,50 @@
 package defrag
 
+import (
+	gocache "github.com/patrickmn/go-cache"
+)
+
+type frame struct {
+	packets []([]byte)
+	id   uint32
+	packetsExpected uint16
+	packetsReceived uint16
+	payloadLen uint16
+
+}
+
+type Defrag struct {
+	lastFrameID uint32
+	frames   gocache.Cache
+	connection  net.PacketConn
+	c chan frame
+}
 
 // Defrag reads fragments of the packets from the connection 
 // collects packets in a cache. When all packets of a frame are collected writes
-// the whole frame to the output
+// the whole frame to the output channel
 // Cutting corners: 
 //  * Assume that all fragments arrive, no timeout 
 //  * No error checks 
 //  * The RAM is unlimited 
-func Defrag(connection net.PacketConn) io.Reader {
+//  * Assume wrap around of the the 32 bits unsigned frame ID
+//  * No initial synchronization: first frame has ID 0 
+func New(func(connection net.PacketConn) io.Reader {
+	d := &Defrag {
+		frames:  gocache.New(gocache.NoExpiration, gocache.NoExpiration),
+		connection: connection,
+		c: make(chan frame)
+	}
+	go func(d *Defrag) {
+
+	}
+
+	return d
+}
+
+// Read reads frames from the channel into the provided buffer
+// Cutting corners:
+//    * ??
+func (d *Defrag) Read(p []byte) (n int, err error) {
 
 }
