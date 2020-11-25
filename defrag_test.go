b086968efd4a65d8fe2467725b22a1fd0662f458
arkady <larytet@yahoo.com>
2020-11-25 20:28:11 +0200

    Update

--- defrag_test.go
+++ defrag_test.go
@@ -23,7 +23,7 @@ type PacketConnMock struct {
 }
 
 func (c *PacketConnMock) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
-	if c.packet > c.packets {
+	if c.packet >= c.packets {
 		return 0, nil, errors.New("EOF")
 	}
 
