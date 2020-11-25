349f183b3c7a9b42f169c3188e5cc6d6fef9b6a8
arkady <larytet@yahoo.com>
2020-11-25 18:49:20 +0200

    Update

--- defrag_test.go
+++ defrag_test.go
@@ -1,22 +1,29 @@
 package defrag
 
 import (
+	"errors"
+	"net"
 	"testing"
 )
 
 type PacketConnMock struct {
 	frame  int
 	packet int
+	packets int
 }
 
-func (c *PacketConnMock) ReadFrom(p []byte) (n int, addr Addr, err error) {
+func (c *PacketConnMock) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	if c.packet > c.packets {
+		return 0, net.Addr{}, errors.New("EOF")
+	}
+
 	packetHeader := PacketHeader {
 		frameID: c.frame,
 		count:   c.packet,
 		number:  3,
 		length:  1,
 	}
-	
+	c.packet += 1
 }
 
 func Test_Read(t *testing.T) {
