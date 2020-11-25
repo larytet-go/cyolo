dbc7e90699608c44b16447c3e5bc2f2e21121330
arkady <larytet@yahoo.com>
2020-11-25 18:42:04 +0200

    Update

--- defrag_test.go
+++ defrag_test.go
@@ -4,8 +4,19 @@ import (
 	"testing"
 )
 
-type struct PacketConn {
+type PacketConnMock struct {
+	frame  int
+	packet int
+}
 
+func (c *PacketConnMock) ReadFrom(p []byte) (n int, addr Addr, err error) {
+	packetHeader := PacketHeader {
+		frameID: c.frame,
+		count:   c.packet,
+		number:  3,
+		length:  1,
+	}
+	
 }
 
 func Test_Read(t *testing.T) {
