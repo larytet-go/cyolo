9da94ff5b3d0962cbc9f6f5ec9d023052e65dc32
arkady <larytet@yahoo.com>
2020-11-25 19:54:42 +0200

    Update

--- defrag_test.go
+++ defrag_test.go
@@ -27,6 +27,7 @@ type PacketConnMock struct {
 }
 
 func (c *PacketConnMock) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	t := c.t
 	if c.packet > c.packets {
 		return 0, nil, errors.New("EOF")
 	}
@@ -41,7 +42,7 @@ func (c *PacketConnMock) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
 	buf := bytes.NewBuffer(p)
 	err = binary.Write(buf, binary.LittleEndian, packetHeader)
 	if err != nil {
-		c.t.Fatalf("Unexpected error %v", err)
+		t.Fatalf("Unexpected error %v", err)
 	}
 	_, packetHeaderSize, _ := getLimits()
 
